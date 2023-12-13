50e957e9807ee4a813d2dc89bb9d270da775c300
lyu-yx <lvyixuan@njust.edu.cn>
2023-12-13 21:09:55 +0800

    loss debug

--- utils/losses.py
+++ utils/losses.py
@@ -20,7 +20,7 @@ def kl_div_loss(fix_pred, gt):
     '''
     KullbackLeibler divergence (KL) 
     '''
-    gt = gt.squeeze()
+    # gt = gt.squeeze()
     # batch_size = fix_pred.size(0)
     # w = fix_pred.size(1)
     # h = fix_pred.size(2)
@@ -59,25 +59,50 @@ def kl_div_loss(fix_pred, gt):
     # return torch.mean(torch.sum(result, 1))
 
 
-def correlation_coefficient_loss(s_map, gt):
+
+def correlation_coefficient_loss(pred, gt):
     '''
     Pearson’s Correlation Coefficient (CC)
     '''
-    batch_size = s_map.size(0)
-    w = s_map.size(1)
-    h = s_map.size(2)
+    # Reshape tensors to [batch_size, -1] to flatten spatial dimensions
+    pred_flat = pred.view(pred.size(0), -1)
+    gt_flat = gt.view(gt.size(0), -1)
 
-    mean_s_map = torch.mean(s_map.view(batch_size, -1), 1).view(batch_size, 1, 1).expand(batch_size, w, h)
-    std_s_map = torch.std(s_map.view(batch_size, -1), 1).view(batch_size, 1, 1).expand(batch_size, w, h)
+    # Compute mean of each tensor along the batch dimension
+    mean_pred = torch.mean(pred_flat, dim=1, keepdim=True)
+    mean_gt = torch.mean(gt_flat, dim=1, keepdim=True)
 
-    mean_gt = torch.mean(gt.view(batch_size, -1), 1).view(batch_size, 1, 1).expand(batch_size, w, h)
-    std_gt = torch.std(gt.view(batch_size, -1), 1).view(batch_size, 1, 1).expand(batch_size, w, h)
+    # Center the tensors by subtracting the mean
+    centered_pred = pred_flat - mean_pred
+    centered_gt = gt_flat - mean_gt
 
-    s_map = (s_map - mean_s_map) / std_s_map
-    gt = (gt - mean_gt) / std_gt
+    # Compute the dot product of the centered tensors
+    dot_product = torch.sum(centered_pred * centered_gt, dim=1)
 
-    ab = torch.sum((s_map * gt).view(batch_size, -1), 1)
-    aa = torch.sum((s_map * s_map).view(batch_size, -1), 1)
-    bb = torch.sum((gt * gt).view(batch_size, -1), 1)
+    # Compute the product of standard deviations
+    std_pred = torch.std(pred_flat, dim=1, keepdim=True)
+    std_gt = torch.std(gt_flat, dim=1, keepdim=True)
 
-    return torch.mean(ab / (torch.sqrt(aa*bb)))
+    # Compute Pearson's correlation coefficient
+    correlation_coefficient = dot_product / (std_pred * std_gt + 1e-8)  # Add small epsilon to avoid division by zero
+
+    return correlation_coefficient.mean().item()
+
+    # batch_size = s_map.size(0)
+    # w = s_map.size(1)
+    # h = s_map.size(2)
+
+    # mean_s_map = torch.mean(s_map.view(batch_size, -1), 1).view(batch_size, 1, 1).expand(batch_size, w, h)
+    # std_s_map = torch.std(s_map.view(batch_size, -1), 1).view(batch_size, 1, 1).expand(batch_size, w, h)
+
+    # mean_gt = torch.mean(gt.view(batch_size, -1), 1).view(batch_size, 1, 1).expand(batch_size, w, h)
+    # std_gt = torch.std(gt.view(batch_size, -1), 1).view(batch_size, 1, 1).expand(batch_size, w, h)
+
+    # s_map = (s_map - mean_s_map) / std_s_map
+    # gt = (gt - mean_gt) / std_gt
+
+    # ab = torch.sum((s_map * gt).view(batch_size, -1), 1)
+    # aa = torch.sum((s_map * s_map).view(batch_size, -1), 1)
+    # bb = torch.sum((gt * gt).view(batch_size, -1), 1)
+
+    # return torch.mean(ab / (torch.sqrt(aa*bb)))
