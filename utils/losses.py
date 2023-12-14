fa7aca107f73b139e3458540b1a363113bf76408
lyu-yx <lvyixuan@njust.edu.cn>
2023-12-14 13:22:35 +0800

    loss debug

--- utils/losses.py
+++ utils/losses.py
@@ -16,10 +16,10 @@ def structure_loss(pred, mask):
     return (wbce + wiou).mean()
 
 
-def kl_div_loss(fix_pred, gt):
+def kl_div_loss(s_map, gt):
     '''
     KullbackLeibler divergence (KL) 
-    '''
+    
     # Reshape tensors to [batch_size, -1] to flatten spatial dimensions
     pred_flat = fix_pred.view(fix_pred.size(0), -1)
     gt_flat = gt.view(gt.size(0), -1)
@@ -32,12 +32,39 @@ def kl_div_loss(fix_pred, gt):
     kl_loss = F.kl_div(torch.log(pred_probs + 1e-8), gt_probs, reduction='mean')
 
     return kl_loss
+    '''
+    s_map = s_map.squeeze()
+    gt = gt.squeeze()
+    batch_size = s_map.size(0)
+    w = s_map.size(1)
+    h = s_map.size(2)
 
+    sum_s_map = torch.sum(s_map.view(batch_size, -1), 1)
+    expand_s_map = sum_s_map.view(batch_size, 1, 1).expand(batch_size, w, h)
+#     print('expand_s_map',expand_s_map.shape)
+#     print('s_map',s_map.shape)
+    assert expand_s_map.size() == s_map.size()
 
-def correlation_coefficient_loss(pred, gt):
+    sum_gt = torch.sum(gt.view(batch_size, -1), 1)
+    expand_gt = sum_gt.view(batch_size, 1, 1).expand(batch_size, w, h)
+    
+    assert expand_gt.size() == gt.size()
+
+    s_map = s_map/(expand_s_map*1.0)
+    gt = gt / (expand_gt*1.0)
+
+    s_map = s_map.view(batch_size, -1)
+    gt = gt.view(batch_size, -1)
+
+    eps = 2.2204e-16
+    result = gt * torch.log(eps + gt/(s_map + eps))
+    # print(torch.log(eps + gt/(s_map + eps))   )
+    return torch.mean(torch.sum(result, 1))
+
+def correlation_coefficient_loss(s_map, gt):
     '''
     Pearson’s Correlation Coefficient (CC)
-    '''
+    
     # Reshape tensors to [batch_size, -1] to flatten spatial dimensions
     pred_flat = pred.view(pred.size(0), -1)
     gt_flat = gt.view(gt.size(0), -1)
@@ -61,3 +88,24 @@ def correlation_coefficient_loss(pred, gt):
     correlation_coefficient = dot_product / (std_pred * std_gt + 1e-8)  # Add small epsilon to avoid division by zero
 
     return correlation_coefficient.mean()
+    '''
+    s_map = s_map.squeeze()
+    gt = gt.squeeze()
+    batch_size = s_map.size(0)
+    w = s_map.size(1)
+    h = s_map.size(2)
+
+    mean_s_map = torch.mean(s_map.view(batch_size, -1), 1).view(batch_size, 1, 1).expand(batch_size, w, h)
+    std_s_map = torch.std(s_map.view(batch_size, -1), 1).view(batch_size, 1, 1).expand(batch_size, w, h)
+
+    mean_gt = torch.mean(gt.view(batch_size, -1), 1).view(batch_size, 1, 1).expand(batch_size, w, h)
+    std_gt = torch.std(gt.view(batch_size, -1), 1).view(batch_size, 1, 1).expand(batch_size, w, h)
+
+    s_map = (s_map - mean_s_map) / std_s_map
+    gt = (gt - mean_gt) / std_gt
+
+    ab = torch.sum((s_map * gt).view(batch_size, -1), 1)
+    aa = torch.sum((s_map * s_map).view(batch_size, -1), 1)
+    bb = torch.sum((gt * gt).view(batch_size, -1), 1)
+
+    return torch.mean(ab / (torch.sqrt(aa*bb)))
